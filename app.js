3a78f138b2fa6ecc4cffc46129c095a10dd5ea33
Abhishekchahar2804 <105548044+Abhishekchahar2804@users.noreply.github.com>
2023-06-05 12:58:32 +0530

    adding simple routing

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyparser=require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next(); 
-});
+app.use(bodyparser.urlencoded({extended:false}))
 
-app.use((req, res, next) => {
-  console.log('In another middleware!');
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="product"></input><input type="text" name="size"></input><button type="sumbit">Add</button></form>')
+})
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/',(req, res, next) => {
   res.send('<h1>Hello from Express!</h1>');
 });
 
