b8e6927ca4b40135ea696a513cb2191a1bb42250
Abhishekchahar2804 <105548044+Abhishekchahar2804@users.noreply.github.com>
2023-06-05 13:33:38 +0530

    make a separate file for admin and user

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const bodyparser=require('body-parser');
-
 const app = express();
 
+
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
 app.use(bodyparser.urlencoded({extended:false}))
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="product"></input><input type="text" name="size"></input><button type="sumbit">Add</button></form>')
+app.use('/admin',adminRoute);
+app.use('/shop',shopRoute);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/')
-})
-
-app.use('/',(req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
 
 app.listen(3000);
